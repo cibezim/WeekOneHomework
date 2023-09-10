31c6a4c8a919b0e9c595cb816441130a9f2fa0eb
cibezim <git@github.com:cibezim/git_test.git>
2023-09-10 17:34:42 -0400

    Initial Commit

---
+++ PhaseMatic/src/main/java/PhaseMatic/PhaseMatic.java
@@ -0,0 +1,42 @@
+package PhaseMatic;
+
+public class PhaseMatic {
+    public static void main(String[] args) {
+
+
+        String[] wordListOne = {
+                "24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win", "front-end",
+                "web-based", "pervasive", "smart", "six-sigma", "critical-path", "dynamic", "energetic"
+        };
+
+        String[] wordListTwo = {
+                "empowered", "sticky", "value-added", "oriented", "centric", "distributed",
+                "clustered", "branded", "outside-the-box", "positioned", "networked", "focused",
+                "leveraged", "aligned", "targeted", "shared", "cooperative", "accelerated", "centered"
+        };
+
+        String[] wordListThree = {
+                "process", "tipping-point", "solution", "architecture", "core competency",
+                "strategy", "mindshare", "portal", "space", "vision", "paradigm", "mission", "plan"
+        };
+
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+
+        String phrase = wordListOne[rand1] + " " +
+                wordListTwo[rand2] + " " +
+                wordListThree[rand3];
+
+
+        System.out.println("What we need is a " + phrase);
+    }
+}
+
